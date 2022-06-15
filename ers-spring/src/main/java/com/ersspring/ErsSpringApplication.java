afb9f94c840e2a39a8763ac229ad95374d99bd6d
navdeep2808 <navdeep.kr21@gmail.com>
2022-06-15 00:17:49 -0400

    Spring Framework Project Skeleton

---
+++ ers-spring/src/main/java/com/ersspring/ErsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ersspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErsSpringApplication.class, args);
+	}
+
+}
